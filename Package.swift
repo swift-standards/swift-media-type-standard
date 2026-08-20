756d5c5991f93da4dba587031aac23c56617a47f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:54:22 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,55 +1,55 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
-  name: "swift-media-type-standard",
-  platforms: [
-    .macOS("27"),
-    .iOS("27"),
-    .tvOS("27"),
-    .watchOS("27"),
-  ],
-  products: [
-    .library(name: "Media Type Standard", targets: ["Media Type Standard"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/swift-ietf/swift-rfc-2045.git", branch: "main"),
-    .package(url: "https://github.com/swift-ietf/swift-rfc-9110.git", branch: "main"),
-  ],
-  targets: [
-    .target(
-      name: "Media Type Standard",
-      dependencies: [
-        .product(name: "RFC 2045", package: "swift-rfc-2045"),
-        .product(name: "RFC 9110", package: "swift-rfc-9110"),
-      ]
-    ),
-    .testTarget(
-      name: "Media Type Standard Tests",
-      dependencies: [
-        "Media Type Standard"
-      ]
-    ),
-  ],
-  swiftLanguageModes: [.v6]
+    name: "swift-media-type-standard",
+    platforms: [
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+    ],
+    products: [
+        .library(name: "Media Type Standard", targets: ["Media Type Standard"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-ietf/swift-rfc-2045.git", branch: "main"),
+        .package(url: "https://github.com/swift-ietf/swift-rfc-9110.git", branch: "main"),
+    ],
+    targets: [
+        .target(
+            name: "Media Type Standard",
+            dependencies: [
+                .product(name: "RFC 2045", package: "swift-rfc-2045"),
+                .product(name: "RFC 9110", package: "swift-rfc-9110"),
+            ]
+        ),
+        .testTarget(
+            name: "Media Type Standard Tests",
+            dependencies: [
+                "Media Type Standard"
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
 )
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
-  let ecosystem: [SwiftSetting] = [
-    .strictMemorySafety(),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("InternalImportsByDefault"),
-    .enableUpcomingFeature("MemberImportVisibility"),
-    .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
-    .enableExperimentalFeature("LifetimeDependence"),
-    .enableExperimentalFeature("Lifetimes"),
-    .enableExperimentalFeature("SuppressedAssociatedTypes"),
-    .enableUpcomingFeature("InferIsolatedConformances"),
-    .enableUpcomingFeature("LifetimeDependence"),
-  ]
+    let ecosystem: [SwiftSetting] = [
+        .strictMemorySafety(),
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+        .enableExperimentalFeature("LifetimeDependence"),
+        .enableExperimentalFeature("Lifetimes"),
+        .enableExperimentalFeature("SuppressedAssociatedTypes"),
+        .enableUpcomingFeature("InferIsolatedConformances"),
+        .enableUpcomingFeature("LifetimeDependence"),
+    ]
 
-  let package: [SwiftSetting] = []
+    let package: [SwiftSetting] = []
 
-  target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
+    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
 }
